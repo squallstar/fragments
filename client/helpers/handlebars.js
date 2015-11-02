33f4a6901bd06e089d340a8f9f957a05db033314
Nicholas Valbusa <squallstar@gmail.com>
2015-11-02 23:23:10 +0000

    moment js

--- client/helpers/handlebars.js
+++ client/helpers/handlebars.js
@@ -1,7 +1,11 @@
-Handlebars.registerHelper('isNull', function (obj) {
-  return obj === null || obj === undefined;
+Handlebars.registerHelper('unless', function (obj) {
+  return !obj;
 });
 
 Handlebars.registerHelper('equals', function (a, b) {
   return a === b;
+});
+
+Handlebars.registerHelper('timeAgo', function (timestamp) {
+  return moment(timestamp).fromNow();
 });
