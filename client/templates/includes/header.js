7ee26338a9326e501e93dfe9e82ba86e8a628534
Nicholas Valbusa <squallstar@gmail.com>
2015-11-05 18:05:11 +0000

    profile picture

--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -1,5 +1,8 @@
 Template.header.helpers({
   displayName: function () {
     return Meteor.user().profile.name;
+  },
+  profilePicture: function () {
+    return Meteor.user().profile.picture;
   }
 });
--- server/lib/accounts.js
+++ server/lib/accounts.js
@@ -9,6 +9,17 @@ Meteor.startup(function() {
     secret: Meteor.settings.googleClientSecret
   });
 
+  Accounts.onCreateUser(function (options, user) {
+    if (user.services.google !== undefined) {
+      if (!user.profile) {
+        user.profile = {};
+      }
+      user.profile.picture = user.services.google.picture;
+    }
+
+    return user;
+  });
+
   Accounts.urls.resetPassword = function (token) {
     return Router.url('reset_password', { token: token });
   };
