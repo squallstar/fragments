2b28bf784f5ca5365a385c082485533500fb769b
Nicholas Valbusa <squallstar@gmail.com>
2015-11-16 17:51:52 +0000

    some tweaks

--- client/lib/ui/autofocus.js
+++ client/lib/ui/autofocus.js
@@ -1,13 +1,12 @@
 UI.setAutofocus = function (template, sessionKey) {
   template.events({
     'click': function (event) {
-      event.preventDefault();
-      event.stopPropagation();
-
       var href = $(event.currentTarget).attr('href');
       if (href && href.substr(0,1) === '/') {
         Session.set(sessionKey, false);
         Router.go(href);
+      } else {
+        event.stopPropagation();
       }
     }
   });
