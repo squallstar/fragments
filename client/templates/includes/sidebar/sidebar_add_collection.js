a7c6cee556e65801fc3ee233dfdb01a1de0aad2f
Nicholas Valbusa <squallstar@gmail.com>
2015-11-17 16:20:40 +0000

    colors on collection

--- client/templates/includes/sidebar/sidebar_add_collection.js
+++ client/templates/includes/sidebar/sidebar_add_collection.js
@@ -2,19 +2,32 @@ Template.sidebarAddCollection.onRendered(function () {
   this.$('input[name="name"]').focus();
 });
 
+Template.sidebarAddCollection.helpers({
+  colors: function () {
+    return _.keys(COLOR_THEMES);
+  }
+});
+
 Template.sidebarAddCollection.events({
   'submit form': function (event, template) {
     event.preventDefault();
 
     var collection = {
-      name: template.$('input[name="name"]').val()
+      name: template.$('input[name="name"]').val(),
+      color: template.$('input[name="color"]:checked').val() || undefined
     };
 
     if (!collection.name) {
       return Notifications.error('You need to specify a name for the collection.');
     }
 
-    Meteor.call('collectionInsert', collection);
-    Session.set(SIDEBAR_ADDING_COLLECTION, false);
+    Meteor.call('collectionInsert', collection, function (err, id) {
+      if (err) {
+        return Notifications.error(err);
+      }
+
+      Session.set(SIDEBAR_ADDING_COLLECTION, false);
+      Router.go('collection', { _id: id });
+    });
   }
 });
