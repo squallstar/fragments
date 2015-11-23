ef0c353a6974cd9b18c7cfb946b5098aa34ca72c
Nicholas Valbusa <squallstar@gmail.com>
2015-11-23 12:18:30 +0000

    export refactor

--- server/lib/export.js
+++ server/lib/export.js
@@ -1,15 +1,13 @@
 Meteor.methods({
   exportData: function () {
-    console.log(Meteor.userId());
     check(Meteor.userId(), String);
 
-    var collections = Collections.find({ user: Meteor.userId() }).map(function (collection) {
-      collection.fragments = Fragments.find({ 'collections._id': collection }).fetch();
-      return collection;
-    });
+    var collections = Collections.find({ user: Meteor.userId() }).fetch(),
+        collectionsIds = _.map(collections, (c) => { return c._id }),
+        fragments = Fragments.find({ 'collections._id': { $in: collectionsIds } }).fetch();
 
     return {
-      collections
+      collections, fragments
     }
   }
 });
