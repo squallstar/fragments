48d2a3c1de3c08cdfc92b180626d888bb849358b
Nicholas Valbusa <squallstar@gmail.com>
2015-11-25 17:20:04 +0000

    reset password template

--- server/lib/accounts/email.js
+++ server/lib/accounts/email.js
@@ -1,5 +1,6 @@
 // Compile email templates
-SSR.compileTemplate('verifyEmail', Assets.getText('emails/verify.html'));
+SSR.compileTemplate('verifyEmail', Assets.getText('emails/verify-email.html'));
+SSR.compileTemplate('resetPassword', Assets.getText('emails/reset-password.html'));
 
 Meteor.startup(function() {
   // Email SMTP setup
@@ -30,6 +31,17 @@ Meteor.startup(function() {
   // Overrides the verify email url
   Accounts.urls.verifyEmail = function (token) {
     return Router.url('verify_email', { token: token });
+  };
+
+  Accounts.emailTemplates.resetPassword.subject = function (user) {
+    return 'Reset your Password';
+  };
+
+  Accounts.emailTemplates.resetPassword.html = function (user, url) {
+    return SSR.render('resetPassword', {
+      name: user.profile.name,
+      url: url
+    });
   };
 
   // Overrides the reset password url
