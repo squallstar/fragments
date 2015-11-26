ea7e79106dcf5c0ed606744ad9e26c1843b837a1
Nicholas Valbusa <squallstar@gmail.com>
2015-11-26 18:08:32 +0000

    seo

---
+++ client/lib/seo.js
@@ -0,0 +1,12 @@
+// Docs: https://atmospherejs.com/lookback/seo
+Router.plugin('seo', {
+  defaults: {
+    title: 'Your fragments',
+    suffix: 'Fragments',
+    separator: '-',
+    description: 'Lorem ipsum dolor sit amet',
+    meta: {
+      keywords: ['fragments']
+    }
+  }
+});
