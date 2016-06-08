9c0b7045f7b100cb5ec8b5110175969bb2b319e0
Nicholas Valbusa <squallstar@gmail.com>
2016-06-08 12:53:13 +0200

    automatically read notifications when reading comments

--- server/publications/comments.js
+++ server/publications/comments.js
@@ -1,6 +1,17 @@
 Meteor.publish('fragmentComments', function (fragmentId) {
   check(fragmentId, String);
 
+  if (this.userId) {
+    Notifications.update({
+      user: this.userId,
+      resource: fragmentId
+    }, {
+      $set: { read_at: Date.now() }
+    }, {
+      multi: true
+    });
+  }
+
   return Comments.find({
     fragment: fragmentId
   });
