2bbf2b2fe2bd886d74a34bfac2dc607c5853023d
Nicholas Valbusa <squallstar@gmail.com>
2016-07-29 19:13:23 +0200

    background color tweaks on sidebar collections

--- client/templates/includes/left-sidebar/sidebar_collection.js
+++ client/templates/includes/left-sidebar/sidebar_collection.js
@@ -25,6 +25,17 @@ Template.sidebarCollection.helpers({
 });
 
 Template.sidebarCollection.events({
+  'mouseenter .item': function (event, template) {
+    var $el = $(event.target),
+        color = $el.find('> span').css('background-color');
+
+    if (color) {
+      $el.css('background-color', shadeColor(rgb2hex(color), 0.8));
+    }
+  },
+  'mouseleave .item': function (event) {
+    $(event.target).css('background-color', 'transparent');
+  },
   'click [data-leave-collection]': function (event) {
     event.preventDefault();
     event.stopPropagation();
@@ -34,3 +45,19 @@ Template.sidebarCollection.events({
     }
   }
 });
+
+function rgb2hex (rgb) {
+  if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
+  rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+
+  function hex(x) {
+      return ('0' + parseInt(x).toString(16)).slice(-2);
+  }
+
+  return '#' + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
+
+function shadeColor (color, percent) {
+    var f = parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+    return "#" + (0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+}
