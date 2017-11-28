30be288cf547d554bd851b73f726f41d9637a2e7
Nicholas Valbusa <squallstar@gmail.com>
2017-11-28 17:09:07 +0100

    tags for #24 and #25

--- client/templates/fragments/add_fragment_popup.js
+++ client/templates/fragments/add_fragment_popup.js
@@ -1,13 +1,60 @@
 // This is being used by the Chrome Plugin
 
 Template.addFragmentPopup.helpers({
+  collectionsSaved: function () {
+    return Template.instance().collectionsSaved.get();
+  },
+  fragmentId: function () {
+    return Template.instance().fragmentId.get();
+  },
   fragment: function () {
     return Fragments.findOne(Template.instance().fragmentId.get());
+  },
+  collections: function () {
+    return Meteor.visibleCollections();
+  },
+  isSelected: function (collection) {
+    var selected = Template.instance().selectedCollections.list();
+    return selected.indexOf(collection._id) !== -1;
+  },
+  hasSelectedCollections: function () {
+    return Template.instance().selectedCollections.list().length > 0;
+  }
+});
+
+Template.addFragmentPopup.events({
+  'click .collections li a': function (event, template) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var id = this._id;
+
+    if (template.selectedCollections.array().indexOf(id) === -1) {
+      template.selectedCollections.push(id);
+    } else {
+      template.selectedCollections.remove(id);
+    }
+  },
+  'click [data-save]': function (event, template) {
+    var collections = Collections.find({
+      _id: { $in: template.selectedCollections.array() }
+    }).map(function (collection) {
+      return collection;
+    });
+
+    Meteor.call('fragmentUpdate', template.fragmentId.get(), {
+      collections: collections
+    }, function () {
+      template.collectionsSaved.set(true);
+      window.close();
+    });
   }
 });
 
 Template.addFragmentPopup.onCreated(function () {
   this.fragmentId = new ReactiveVar();
+  this.selectedCollections = new ReactiveArray();
+  this.collectionsSaved = new ReactiveVar(false);
 
   Meteor.call('fragmentInsert', { url: this.data }, (error, fragmentId) => {
     if (error) {
